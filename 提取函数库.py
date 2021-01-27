0da7f5bcd0432861273301a495b291c1a89202cf
Vector-S <jiraiya812@gmail.com>
2021-01-26 22:15:32 -0500

    Complete test framework and added more extract functions

--- 提取函数库.py
+++ 提取函数库.py
@@ -3,45 +3,70 @@ import re
 
 def 提取地点(input):
     re_templates = {
-        r"^(.*)[省|市].*法院"
+        r"(\S*)[省|市].*法院.*"
     }
     for template in re_templates:
-        m = re.compile(template).match(input)
+        m = re.search(template, input, re.M + re.X)
         if m:
             return m.groups()[0]
+    return "未知"
 
 def 测试提取地点():
-    assert 提取地点("四川省珙县人民法院") == "四川"
+    assert 提取地点("\n 四川省珙县人民法院\n") == "四川"
     assert 提取地点("湖南省珙县人民法院") == "湖南"
     assert 提取地点("北京市朝阳区人民法院") == "北京"
 
 def 提取犯罪数额(input):
-    return
+    return "未知"
+
+def 布尔编码(value):
+    return "有" if value else "无"
 
 
 def 提取前科(input):
     result = False
-    if re.compile("无前科|初犯").match(input):
+    if re.search("无前科|初犯",input):
         result = False
-    if re.compile("有前科|再犯").match(input):
+    if re.search("有前科|再犯|有前科劣迹",input):
         result = True
-    return result
+    return 布尔编码(result)
 
 
 def 提取自首(input):
-    return
-
+    result = False
+    if re.search("自首",input):
+        result = True
+    return 布尔编码(result)
 
 def 提取坦白(input):
-    return
+    result = False
+    if re.search("坦白",input):
+        result = True
+    return 布尔编码(result)
 
 
 def 提取退赔(input):
-    return
+    result = False
+    if re.search("退赔", input):
+        result = True
+        return "有"
+
+    return "未知"
 
 
 def 提取认罪认罚(input):
+    result = False
+    if re.search("认罪|认罚", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取自由刑(input):
+
     return
+
+def 提取财产刑(input):
+    result = re.search("罚金人民币(.*)元",input)
+    return result.groups()[0]
 
 if __name__ == "__main__":
     测试提取地点()
