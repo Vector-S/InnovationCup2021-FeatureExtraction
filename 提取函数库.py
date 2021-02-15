1ac36ae7dea119682254592672627dbae89719f5
Vector-S <jiraiya812@gmail.com>
2021-02-15 02:40:50 -0500

    finish the extractoin logics, v1

--- 提取函数库.py
+++ 提取函数库.py
@@ -1,5 +1,6 @@
 import re
-
+from datetime import date
+import pycnnum
 
 def 提取性别(input):
     re_templates = [
@@ -12,18 +13,20 @@ def 提取性别(input):
     return "未知"
 
 def 提取年龄(input):
-    m = re.search(r"(0-9)岁", input, re.M + re.X)
+    m = re.search(r"(\d*)年(\d*)月(\d*)日.*出?生于?", input, re.M + re.X)
+    if m:
+        DOB = date(int(m.groups()[0]),int(m.groups()[1]),int(m.groups()[2]))
+        result = (date.today()- DOB).days//365
+        return str(result)
+    m = re.search(r"(\d*)岁", input, re.M + re.X)
     if m:
         return m.groups()[0]
-    m = re.search(r"([0-9]*)年.*出生", input, re.M + re.X)
-    if m:
-        year_of_birth = int(m.groups()[0])
-        return 2021 - year_of_birth - 1
     return "未知"
 
 def 提取文化程度(input):
     re_templates = [
-        r"(小学|初中|高中|本科|研究生)文化",
+        r"(小学|初中|高中|本科|中专|大专|专科|大学|研究生|博士)(毕业|文化|肄业)",
+        r"(文盲)",
     ]
     for template in re_templates:
         m = re.search(template, input, re.M + re.X)
@@ -31,11 +34,35 @@ def 提取文化程度(input):
             return m.groups()[0]
     return "未知"
 
+
+def 提取聋哑(input):
+    result = False
+    if re.search("聋|哑",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取盲人(input):
+    result = False
+    if re.search("盲人|失明",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取残疾人(input):
+    result = False
+    if re.search("残疾|残障",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取精神病人(input):
+    result = False
+    if re.search("精神病",input):
+        result = True
+    return 布尔编码(result)
 
 def 提取地区(input):
+    all_value = '|'.join(PROVINCE_DICT.keys())
     re_templates = [
-        r":(\S*)[省|市].*法院.*",
-        r"(\S*)[省|市].*法院.*"
+        r"({})[省|市].*法院.*".format(all_value)
     ]
     for template in re_templates:
         m = re.search(template, input, re.M + re.X)
@@ -43,24 +70,25 @@ def 提取地区(input):
             return m.groups()[0]
     return "未知"
 
-def 测试提取地点():
-    assert 提取地点("\n 四川省珙县人民法院\n") == "四川"
-    assert 提取地点("湖南省珙县人民法院") == "湖南"
-    assert 提取地点("北京市朝阳区人民法院") == "北京"
-    assert 提取地点("审理法院:重庆市垫江县人民法院") == "重庆"
+PROVINCE_DICT = {
+    '北京': 11, '天津': 12, '河北': 13, '山西': 14, '内蒙古': 15,
+    '辽宁': 21, '吉林': 22, '黑龙江': 23,
+    '上海': 31, '江苏': 32, '浙江': 33, '安徽': 34, '福建': 35, '江西': 36, '山东': 37,
+    '河南': 41, '湖北': 42, '湖南': 43, '广东': 44, '广西': 45, '海南': 46,
+    '重庆': 50, '四川': 51, '贵州': 52, '云南': 53, '西藏': 54,
+    '陕西': 61, '甘肃': 62, '青海': 63, '宁夏': 64, '新疆': 65
+}
 
-def 提取盗窃数额(input):
-    return "未知"
+def 测试提取地区():
+    assert 提取地区("\n 四川省珙县人民法院\n") == "四川"
+    assert 提取地区("湖南省珙县人民法院") == "湖南"
+    assert 提取地区("北京市朝阳区人民法院") == "北京"
+    assert 提取地区("审理法院:重庆市垫江县人民法院") == "重庆"
 
-def 布尔编码(value):
-    return "有" if value else "无"
 
-
-def 提取前科(input):
+def 提取被害人谅解(input):
     result = False
-    if re.search("无前科|初犯",input):
-        result = False
-    if re.search("有前科|再犯|有前科劣迹",input):
+    if re.search("被害人谅解",input):
         result = True
     return 布尔编码(result)
 
@@ -73,35 +101,390 @@ def 提取自首(input):
 
 def 提取坦白(input):
     result = False
-    if re.search("坦白",input):
+    if re.search("坦白|如实供述", input):
         result = True
     return 布尔编码(result)
 
 
-def 提取退赔(input):
+def 提取认罪态度良好(input):
     result = False
-    if re.search("退赔", input):
+    if re.search("认罪态度良好|态度良好",input):
         result = True
-        return "有"
-
-    return "未知"
-
+    return 布尔编码(result)
 
 def 提取认罪认罚(input):
     result = False
-    if re.search("认罪|认罚", input):
+    if re.search("认罪|认罚|如实供述", input):
         result = True
     return 布尔编码(result)
 
-def 提取自由刑(input):
+
+def 提取立功(input):
+    result = False
+    if re.search("立功", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取退赔(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[0]
+    result = False
+    if re.search("退赔|退赃|退还|赔偿", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取自然灾害(input):
+    result = False
+    if re.search("自然灾害|事故灾害|社会安全事件|突发事件", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取在医院盗窃(input):
+    result = False
+    if re.search("医院盗窃|在医院盗窃", input):
+        result = True
+    return 布尔编码(result)
+
+
+def 提取盗窃残疾人(input):
+    result = False
+    if re.search("盗窃残疾人|孤寡老人|丧失劳动能力", input):
+        result = True
+    return 布尔编码(result)
+
+
+
+def 提取盗窃救灾款物(input):
+    result = False
+    if re.search("救灾款物|移民|扶贫", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取一年内盗窃(input):
+    result = False
+    if re.search("一年内盗窃", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取造成严重后果(input):
+    result = False
+    if re.search("成严重后果", input):
+        result = True
+    return 布尔编码(result)
+
+def 提取前科(input):
+    result = False
+    if re.search("无.*前科|没有.*前科|初犯",input):
+        result = False
+    if re.search("曾因犯|[^没]有.*犯?罪?前科|有前科劣迹|累犯|曾被判|日因犯.*罪|因犯.*罪\,于|刑满释放",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取累犯(input):
+    result = False
+    if re.search("累犯",input):
+        result = True
+    return 布尔编码(result)
+
+
+
+def IsNum(s):
+    s = s.replace(',','')
+    if not s:
+        return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        result = pycnnum.cn2num(s)
+        return result != 0
+    except Exception:
+        return False
+
+def ToNum(s):
+    s = s.replace(',','')
+    try:
+        result = int(float(s))
+        return result
+    except:
+        pass
+    try:
+        result = pycnnum.cn2num(s)
+        return result
+    except Exception as e:
+        raise e
+
+def 提取盗窃数额(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[0]
+    # 共计
+    re_templates = [
+        "共计(\d*)余?元",
+        "共计人民币(\d*)余?元",
+        "共计价值人民币(\d*)余?元",
+        "金额共计(\d*)余?元",
+        "涉案人民币(\d*)余?元",
+        "经鉴定共计人民币(\d*)余?元",
+        "盗窃.*共计人民币(\d*)余?元",
+        "被[盗|窃|偷].*总价格为人民币(\d*)余?元",
+        "累计.*人民币(\d*)余?元",
+        "[盗|窃|偷][得|取].*共计人民币(\d*)余?元",
+        "总价.*人民币(\d*)余?元",
+    ]
+    for template in re_templates:
+        m = re.findall(template, input, re.M + re.X)
+
+        if m and IsNum(m[0]):
+            m = [ToNum(m[0]) for v in m]
+            return max(m)
+    # 分计
+    re_templates = [
+        "被[盗|窃|偷].*价值.*人民币(.*)余?元",
+        "[盗|窃|偷]取.*(\d*)余?元",
+        "价值(\d*)余?元",
+        "价值人民币(\d*)余?元",
+        "[盗|窃|偷]人民币(\d*)余?元",
+    ]
+    for template in re_templates:
+        m = re.findall(template, input, re.M + re.X)
+        if m:
+            result = 0
+            for value in m:
+                if IsNum(value):
+                    result += ToNum(value)
+            return result
+    return 0
+
+def 测试提取盗窃数额():
+    提取盗窃数额(",七部被盗手机经鉴定共计人民币4620元。\n") == 4620
 
     return
+def 提取入户(input):
+    result = False
+    if re.search("入户",input):
+        result = True
+    return 布尔编码(result)
 
-def 提取财产刑(input):
-    result = re.search("罚金人民币(.*)元",input)
-    if result:
-        return result.groups()[0]
+def 提取携带凶器(input):
+    result = False
+    if re.search("凶器",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取扒窃(input):
+    result = False
+    if re.search("扒窃",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取破坏性手段(input):
+    result = False
+    if re.search("破坏性手段",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取未遂(input):
+    result = False
+    if re.search("未遂",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取中止(input):
+    result = False
+    if re.search("中止",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取多次盗窃(input):
+    result = False
+    if re.search("多次盗窃",input):
+        result = True
+    return 布尔编码(result)
+
+
+
+
+def 提取免于刑事处罚(input):
+    result = False
+    if re.search("免于刑事处罚",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取拘役(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    if "数罪并罚" in input:
+        input = re.split("数罪并罚",input,1)[-1]
+
+    re_templates = [
+        "有期徒刑(.*)年(.*)月",
+        "有期徒刑(.*)年",
+        "有期徒刑(.*)月",
+    ]
+
+    m = re.search("拘役([一二两三四五六七八九十]*)年零?([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m and len(m.groups()) == 2:
+        year, month = m.groups()[0], m.groups()[1]
+        year = ToNum(year)
+        month = ToNum(month)
+        return "{}Y{}M".format(year,month)
+
+    m = re.search("拘役([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m:
+        month = m.groups()[0]
+        month = ToNum(month)
+        return "{}M".format(month)
+
+    m = re.search("拘役([一二两三四五六七八九十]*)年", input, re.M + re.X)
+    if m:
+        year = m.groups()[0]
+        year = ToNum(year)
+        return "{}Y".format(year)
+    return 0
+
+def 提取管制(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    if "数罪并罚" in input:
+        input = re.split("数罪并罚",input,1)[-1]
+
+    re_templates = [
+        "有期徒刑(.*)年(.*)月",
+        "有期徒刑(.*)年",
+        "有期徒刑(.*)月",
+    ]
+
+    m = re.search("有期管制([一二两三四五六七八九十]*)年零?([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m and len(m.groups()) == 2:
+        year, month = m.groups()[0], m.groups()[1]
+        year = ToNum(year)
+        month = ToNum(month)
+        return "{}Y{}M".format(year,month)
+
+    m = re.search("有期管制([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m:
+        month = m.groups()[0]
+        month = ToNum(month)
+        return "{}M".format(month)
+
+    m = re.search("有期管制([一二两三四五六七八九十]*)年", input, re.M + re.X)
+    if m:
+        year = m.groups()[0]
+        year = ToNum(year)
+        return "{}Y".format(year)
+    return 0
+
+def 提取有期徒刑(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    if "数罪并罚" in input:
+        input = re.split("数罪并罚",input,1)[-1]
+
+    m = re.search("有期徒刑([一二两三四五六七八九十]*)年零?([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m and len(m.groups()) == 2:
+        year, month = m.groups()[0], m.groups()[1]
+        year = ToNum(year)
+        month = ToNum(month)
+        return "{}Y{}M".format(year,month)
+
+    m = re.search("有期徒刑([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m:
+        month = m.groups()[0]
+        month = ToNum(month)
+        return "{}M".format(month)
+
+    m = re.search("有期徒刑([一二两三四五六七八九十]*)年", input, re.M + re.X)
+    if m:
+        year = m.groups()[0]
+        year = ToNum(year)
+        return "{}Y".format(year)
+    return 0
+
+def 提取罚金(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    if "数罪并罚" in input:
+        input = re.split("数罪并罚",input,1)[-1]
+    # 数字罚金
+    re_templates = [
+        "罚金人民币(.*)元",
+    ]
+    for template in re_templates:
+        m = re.findall(template, input, re.M + re.X)
+
+        if m:
+            result = m[0].replace(',', '')
+            if IsNum(result):
+                return ToNum(result)
+    # 文本罚金
+    re_templates = [
+        "罚金人民币(.*)元",
+    ]
+    for template in re_templates:
+        m = re.findall(template, input, re.M + re.X)
+        if m:
+            return m[0]
     return "未知"
 
+def 提取没收个人财产(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    result = False
+    if re.search("没收个人财产",input):
+        result = True
+    return 布尔编码(result)
+
+def 提取没收个人财产数额(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    if "数罪并罚" in input:
+        input = re.split("数罪并罚",input,1)[-1]
+
+    re_templates = [
+        "没收个人财产人民币(.*)元",
+    ]
+    for template in re_templates:
+        m = re.findall(template, input, re.M + re.X)
+
+        if m:
+            result = m[0].replace(',', '')
+            if IsNum(result):
+                return ToNum(result)
+    return 0
+
+def 提取缓刑(input):
+    if "判决如下" in input:
+        input = re.split("判决如下",input,1)[-1]
+    if "数罪并罚" in input:
+        input = re.split("数罪并罚",input,1)[-1]
+
+    m = re.search("缓刑([一二两三四五六七八九十]*)年零?([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m and len(m.groups()) == 2:
+        year, month = m.groups()[0], m.groups()[1]
+        year = ToNum(year)
+        month = ToNum(month)
+        return "{}Y{}M".format(year,month)
+
+    m = re.search("缓刑([一二两三四五六七八九十]*)个?月", input, re.M + re.X)
+    if m:
+        month = m.groups()[0]
+        month = ToNum(month)
+        return "{}M".format(month)
+
+    m = re.search("缓刑([一二两三四五六七八九十]*)年", input, re.M + re.X)
+    if m:
+        year = m.groups()[0]
+        year = ToNum(year)
+        return "{}Y".format(year)
+    return 0
+
+
+
+def 布尔编码(value):
+    return "是" if value else "否"
+
 if __name__ == "__main__":
-    测试提取地点()
+    测试提取地区()
+    测试提取盗窃数额()
--- 特征提取.py
+++ 特征提取.py
@@ -4,46 +4,98 @@ import re
 from 提取函数库 import *
 
 TEST = True
+CLEAN = False
 PROD = False
 
 # 0 1 2 3
 VERBOSE  = 0
 
-MAX_FILE_COUNT = 200
+MAX_FILE_COUNT = 100
+
+target_files = []
+
+FOCUS_FEATURE = ["文化程度", "地区", "认罪态度良好", "前科"]
+
+
+
+FOCUS_FEATURE = ["罚金"]
+
+FOCUS_FEATURE = ["有期徒刑","拘役","管制"]
+
+FOCUS_FEATURE = ["年龄"]
 
 if TEST:
     input_file_path = './testset_v1'
+    # target_files = ["88.txt"]
+
+if CLEAN:
+    clean_file_path = './ilegalfiles'
 
 if PROD:
     input_file_path = './rawdata'
 
-
-
 def unify_feature_name(name):
     if name == "地点":
         name = "地区"
+    if name == "罚金刑":
+        name = "罚金"
+    if name in ["退赃退赔"]:
+        name = "退赔"
+    if name == "自然灾害、事故灾害、社会安全事件等突发事件期间,在事件发生地盗窃":
+        name = "自然灾害"
+    if name == "在医院盗窃病人或者其亲友财物的":
+        name = "在医院盗窃"
+    if name == "盗窃残疾人、孤寡老人、丧失劳动能力人的财物的":
+        name = "盗窃残疾人"
+    if name == "盗窃救灾、抢险、防汛、优抚、扶贫、移民、救济款物的":
+        name = "盗窃救灾款物"
+    if name == "一年内曾因盗窃受过行政处罚":
+        name = "一年内盗窃"
+    if name == "因盗窃造成严重后果的":
+        name = "造成严重后果"
+    if name == "携带凶器盗窃":
+        name = "携带凶器"
     return name
+
+
+
+def unify_feature_value(name,value):
+    if value in ["是", "有"]:
+        value = "是"
+    if value in ["无", "否"]:
+        value = "否"
+    if value in [""]:
+        value = "未知"
+    if IsNum(value):
+        value = ToNum(value)
+    if name in ["盗窃数额","缓刑","没收个人财产数额"] and  value in ["未知", "否", "无"]:
+        value = 0
+    return value
 
 def readTestFile(file_path):
     # 读取测试集，用来迭代开发时验证程序的正确性
     if VERBOSE>=0: print("Reading " + file_path)
     raw_text = open(file_path,'r').read()
-    result = re.split(r"\#预期特征\n",raw_text,1)
+    result = re.split(r"\#\s*预期特征\s*\n",raw_text,1)
     try:
         article_section, feature_section = result
     except Exception as e:
         print("无法读取预期特征 {}".format(file_path))
+        return
     features = {}
+    reasons = {}
     for line in feature_section.split('\n'):
         line = line.strip()
         if line and not re.match(r'\s*#',line) and line.count('|') >= 2:
-            feature_name, feature_value, rest = re.split(r"\s*\|\s*", line, 3)
-            # feature_name = unify_feature_name(feature_name)
+            feature_name, feature_value, reason = re.split(r"\s*\|\s*", line, 2)
+            feature_name = unify_feature_name(feature_name)
+            feature_value = unify_feature_value(feature_name, feature_value)
             features[feature_name] = feature_value
+            reasons[feature_name] = reason
         else:
             # 无法读取
             pass
-    return article_section, features
+    return article_section, features, reasons
 
 
 def readRawFile(filepath):
@@ -55,22 +107,57 @@ def readRawFile(filepath):
 # 地点
 
 # 为每一个特征单独创建特征提取函数
-feature_fun_map = {
-                    "性别": 提取性别,
-                    "年龄": 提取年龄,
-                    "地区": 提取地区,
-                    "文化程度" : 提取文化程度,
-                    "盗窃数额": 提取盗窃数额,
-                    "前科": 提取前科,
-                    "自首": 提取自首,
-                    "坦白": 提取坦白,
-                    "退赔": 提取退赔,
-                    "认罪认罚": 提取认罪认罚,
-                    "罚金": 提取财产刑,}
+
+feature_fun_map ={
+        "性别" : 提取性别,
+        "年龄" : 提取年龄,
+        "文化程度" : 提取文化程度,
+        "聋哑" : 提取聋哑,
+        "盲人" : 提取盲人,
+        "残疾人" : 提取残疾人,
+        "精神病人" : 提取精神病人,
+        "地区" : 提取地区,
+        "认罪认罚" : 提取认罪认罚,
+        "被害人谅解" : 提取被害人谅解,
+        "自首" : 提取自首,
+        "坦白" : 提取坦白,
+        "认罪态度良好" : 提取认罪态度良好,
+        "立功" : 提取立功,
+        "退赔" : 提取退赔,
+        "自然灾害" : 提取自然灾害,
+        "在医院盗窃" : 提取在医院盗窃,
+        "盗窃残疾人" : 提取盗窃残疾人,
+        "盗窃救灾款物" : 提取盗窃救灾款物,
+        "一年内盗窃" : 提取一年内盗窃,
+        "造成严重后果" : 提取造成严重后果,
+        "前科" : 提取前科,
+        "累犯" : 提取累犯,
+        "盗窃数额" : 提取盗窃数额,
+        "入户" : 提取入户,
+        "携带凶器" : 提取携带凶器,
+        "扒窃" : 提取扒窃,
+        "破坏性手段" : 提取破坏性手段,
+        "未遂" : 提取未遂,
+        "中止" : 提取中止,
+        "多次盗窃" : 提取多次盗窃,
+        "免于刑事处罚" : 提取免于刑事处罚,
+        "拘役" : 提取拘役,
+        "管制" : 提取管制,
+        "有期徒刑" : 提取有期徒刑,
+        "罚金" : 提取罚金,
+        "没收个人财产" : 提取没收个人财产,
+        "没收个人财产数额" : 提取没收个人财产数额,
+        "缓刑" : 提取缓刑,
+        }
 
 accuracy_stats = { name: [0,0] for name in feature_fun_map.keys()}
 
 
+def format_function_names(keys):
+    print("{")
+    for key in keys:
+        print("\t\"{}\" : 提取{},".format(key,key))
+    print("}")
 
 def 统计判决如下(txts):
     rate = sum([ 1 for txt in txts if "判决如下" in txt])/len(txts)
@@ -80,30 +167,39 @@ if __name__ == "__main__":
     print("数据读取路径: ",input_file_path)
     if TEST:
         for filename in os.listdir(input_file_path)[:MAX_FILE_COUNT]:
+            if target_files and filename not in target_files:
+                continue
             extracted_features = {}
             try:
-                article_section, expected_features = readTestFile(os.path.join(input_file_path,filename))
+                article_section, expected_features, reasons = readTestFile(os.path.join(input_file_path,filename))
             except Exception as e:
-                print("测试集无法识别: {}".format(filename))
+                print("测试集无法识别: {} {}".format(filename,e))
                 continue
             for key, fun in feature_fun_map.items():
                 extracted_features[key] = fun(article_section)
             # RE 调试点
             temp = re.search(r"(\S*)[省|市].*法院.*", article_section, re.M + re.X)
-            if VERBOSE>=1: print("特征数量: {}".format(len(expected_features)))
+
+            extracted_features_count = 39
+            # format function names
+            # format_function_names(expected_features.keys())
+            if len(expected_features) !=  extracted_features_count:
+                print("{} 特征数不符， 读取 {}， 预期 {}".format(filename,len(expected_features), extracted_features_count))
+                if CLEAN:
+                    os.rename(os.path.join(input_file_path,filename),os.path.join(clean_file_path,filename))
+
             for name in feature_fun_map.keys():
                 if name not in extracted_features:
                     print("Feature not extracted: {}".format(name))
                     continue
                 accuracy_stats[name][1] += 1
                 if name not in expected_features:
-                    print("Unexpected feature: {}".format(name))
+                    print("Unexpected feature {} in file {}".format(name,filename))
                     continue
                 value = expected_features[name]
-                if name not in extracted_features:
-                    if VERBOSE>=1: print("无法提取 {},\t     预期值 {}".format(name, value))
-                elif extracted_features[name] != value:
-                    if VERBOSE>=1: print("提取错误 {},\t     预期值 {},\t     提取值 {}".format(name, value, extracted_features[name]))
+                if str(extracted_features[name]) != str(value):
+                    if VERBOSE>=1 or name in FOCUS_FEATURE :
+                        print("{} 提取错误 {},\t     预期值 {},\t     提取值 {},\t {}".format(filename, name, value, extracted_features[name],reasons[name]))
                 else:
                     accuracy_stats[name][0] += 1
                     if VERBOSE>=1: print("提取正确 {},\t     预期值 {},\t     提取值 {}".format(name, value, extracted_features[name]))
@@ -111,7 +207,7 @@ if __name__ == "__main__":
         统计判决如下(["判决如下", "判决如下1"])
     print("提取准确度")
     for name, stats in accuracy_stats.items():
-        print("{}:\t {}/{} = {}".format(name,stats[0],stats[1],stats[0]/stats[1]))
+        print("{}:\t {}/{} = {:.1f}%".format(name, stats[0], stats[1], stats[0]/stats[1]*100))
     if PROD:
         pass
 
