649d9c699099dec5dd4b36e2d46395f858ba9f13
Vector-S <jiraiya812@gmail.com>
2021-02-14 20:47:39 -0500

    update testset

--- 提取函数库.py
+++ 提取函数库.py
@@ -1,10 +1,42 @@
 import re
 
 
-def 提取地点(input):
-    re_templates = {
+def 提取性别(input):
+    re_templates = [
+        r"(男|女)",
+    ]
+    for template in re_templates:
+        m = re.search(template, input, re.M + re.X)
+        if m:
+            return m.groups()[0]
+    return "未知"
+
+def 提取年龄(input):
+    m = re.search(r"(0-9)岁", input, re.M + re.X)
+    if m:
+        return m.groups()[0]
+    m = re.search(r"([0-9]*)年.*出生", input, re.M + re.X)
+    if m:
+        year_of_birth = int(m.groups()[0])
+        return 2021 - year_of_birth - 1
+    return "未知"
+
+def 提取文化程度(input):
+    re_templates = [
+        r"(小学|初中|高中|本科|研究生)文化",
+    ]
+    for template in re_templates:
+        m = re.search(template, input, re.M + re.X)
+        if m:
+            return m.groups()[0]
+    return "未知"
+
+
+def 提取地区(input):
+    re_templates = [
+        r":(\S*)[省|市].*法院.*",
         r"(\S*)[省|市].*法院.*"
-    }
+    ]
     for template in re_templates:
         m = re.search(template, input, re.M + re.X)
         if m:
@@ -15,8 +47,9 @@ def 测试提取地点():
     assert 提取地点("\n 四川省珙县人民法院\n") == "四川"
     assert 提取地点("湖南省珙县人民法院") == "湖南"
     assert 提取地点("北京市朝阳区人民法院") == "北京"
+    assert 提取地点("审理法院:重庆市垫江县人民法院") == "重庆"
 
-def 提取犯罪数额(input):
+def 提取盗窃数额(input):
     return "未知"
 
 def 布尔编码(value):
@@ -66,7 +99,9 @@ def 提取自由刑(input):
 
 def 提取财产刑(input):
     result = re.search("罚金人民币(.*)元",input)
-    return result.groups()[0]
+    if result:
+        return result.groups()[0]
+    return "未知"
 
 if __name__ == "__main__":
     测试提取地点()
--- 特征提取.py
+++ 特征提取.py
@@ -6,22 +6,43 @@ from 提取函数库 import *
 TEST = True
 PROD = False
 
+# 0 1 2 3
+VERBOSE  = 0
+
+MAX_FILE_COUNT = 200
+
 if TEST:
-    input_file_path = './testset'
+    input_file_path = './testset_v1'
 
 if PROD:
     input_file_path = './rawdata'
 
+
+
+def unify_feature_name(name):
+    if name == "地点":
+        name = "地区"
+    return name
+
 def readTestFile(file_path):
     # 读取测试集，用来迭代开发时验证程序的正确性
+    if VERBOSE>=0: print("Reading " + file_path)
     raw_text = open(file_path,'r').read()
-    article_section, feature_section = re.split(r"#\s*预期特征\s*\n",raw_text,2)
+    result = re.split(r"\#预期特征\n",raw_text,1)
+    try:
+        article_section, feature_section = result
+    except Exception as e:
+        print("无法读取预期特征 {}".format(file_path))
     features = {}
     for line in feature_section.split('\n'):
         line = line.strip()
         if line and not re.match(r'\s*#',line) and line.count('|') >= 2:
             feature_name, feature_value, rest = re.split(r"\s*\|\s*", line, 3)
+            # feature_name = unify_feature_name(feature_name)
             features[feature_name] = feature_value
+        else:
+            # 无法读取
+            pass
     return article_section, features
 
 
@@ -35,41 +56,62 @@ def readRawFile(filepath):
 
 # 为每一个特征单独创建特征提取函数
 feature_fun_map = {
-                    "地点": 提取地点,
-                    "犯罪数额": 提取犯罪数额,
+                    "性别": 提取性别,
+                    "年龄": 提取年龄,
+                    "地区": 提取地区,
+                    "文化程度" : 提取文化程度,
+                    "盗窃数额": 提取盗窃数额,
                     "前科": 提取前科,
                     "自首": 提取自首,
                     "坦白": 提取坦白,
                     "退赔": 提取退赔,
                     "认罪认罚": 提取认罪认罚,
+                    "罚金": 提取财产刑,}
 
-                    "财产刑": 提取财产刑,}
+accuracy_stats = { name: [0,0] for name in feature_fun_map.keys()}
+
+
 
 def 统计判决如下(txts):
     rate = sum([ 1 for txt in txts if "判决如下" in txt])/len(txts)
     print("{}% 的文章含有判决如下".format(rate*100))
 
 if __name__ == "__main__":
-    print("数据读取路径: ",os.getcwd())
+    print("数据读取路径: ",input_file_path)
     if TEST:
-        for filename in os.listdir(input_file_path):
+        for filename in os.listdir(input_file_path)[:MAX_FILE_COUNT]:
             extracted_features = {}
-            article_section, expected_features = readTestFile(os.path.join(input_file_path,filename))
-            print(article_section)
+            try:
+                article_section, expected_features = readTestFile(os.path.join(input_file_path,filename))
+            except Exception as e:
+                print("测试集无法识别: {}".format(filename))
+                continue
             for key, fun in feature_fun_map.items():
                 extracted_features[key] = fun(article_section)
             # RE 调试点
             temp = re.search(r"(\S*)[省|市].*法院.*", article_section, re.M + re.X)
-            for name, value in expected_features.items():
+            if VERBOSE>=1: print("特征数量: {}".format(len(expected_features)))
+            for name in feature_fun_map.keys():
                 if name not in extracted_features:
-                    print("无法提取 {},\t     预期值 {}".format(name, value))
+                    print("Feature not extracted: {}".format(name))
+                    continue
+                accuracy_stats[name][1] += 1
+                if name not in expected_features:
+                    print("Unexpected feature: {}".format(name))
+                    continue
+                value = expected_features[name]
+                if name not in extracted_features:
+                    if VERBOSE>=1: print("无法提取 {},\t     预期值 {}".format(name, value))
                 elif extracted_features[name] != value:
-                    print("提取错误 {},\t     预期值 {},\t     提取值 {}".format(name, value, extracted_features[name]))
+                    if VERBOSE>=1: print("提取错误 {},\t     预期值 {},\t     提取值 {}".format(name, value, extracted_features[name]))
                 else:
-                    print("提取正确 {},\t     预期值 {},\t     提取值 {}".format(name, value, extracted_features[name]))
+                    accuracy_stats[name][0] += 1
+                    if VERBOSE>=1: print("提取正确 {},\t     预期值 {},\t     提取值 {}".format(name, value, extracted_features[name]))
         print("\n统计分析\n")
         统计判决如下(["判决如下", "判决如下1"])
-
+    print("提取准确度")
+    for name, stats in accuracy_stats.items():
+        print("{}:\t {}/{} = {}".format(name,stats[0],stats[1],stats[0]/stats[1]))
     if PROD:
         pass
 
